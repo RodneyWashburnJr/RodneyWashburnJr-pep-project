b65a5e8c62f3fa92af6d8ab200ae00b62df73acf
Rodney Washburn <Rodneywashburn01@gmail.com>
2025-01-28 19:45:24 +0000

    Commit Social Media Part 3

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,7 +1,17 @@
 package Service;
 import DAO.AccountDAO;
 import Model.Account;
-import java.util.List;
 public class AccountService {
-    
+    private AccountDAO accountDAO;
+     
+
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+    public Account addAccount(Account account) {
+        return accountDAO.newAccount(account);
+    }
 }
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -3,5 +3,35 @@ import DAO.MessageDAO;
 import Model.Message;
 import java.util.List;
 public class MessageService {
-    
+    public MessageDAO messageDAO;
+
+
+    public MessageService(){
+        messageDAO = new MessageDAO();
+    }
+    public MessageService(MessageDAO messageDAO){
+        this.messageDAO = messageDAO;
+    }
+    public List<Message> getAllMessages() {
+        return messageDAO.getAllMessages();
+    }
+    public Message addMessage(Message message) {
+        Message existingMessage = messageDAO.getMessageByID(message.getMessage_id());
+    if (existingMessage != null) {
+        return null;
+    }
+        return messageDAO.newMessage(message);
+    }
+    public List<Message> getAllMessageByID(int posted_by) {
+        return messageDAO.messagesByUser(posted_by);
+    }
+    public Message getOneMessageByID(int message_id){
+        return messageDAO.getMessageByID(message_id);
+    }
+    public boolean deleteByID(int message_id){
+        return messageDAO.deleteMessagebyID(message_id);
+    }
+    public boolean updateByID(int message_id, String newMessageText){
+        return messageDAO.updateMessageByID(message_id, newMessageText);
+    }
 }
